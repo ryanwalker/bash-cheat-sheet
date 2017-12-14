c2327c4f6ca64363de8ee62f4c08f5590d5854c6
Ryan Walker <ryan.walker@kubra.com>
2017-12-14 10:26:51 -0700

    note about -C

--- bash.sh
+++ bash.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # Curl and extract at once
+#										   -C changes to dir before extracting
 curl https://s3-us-west-2.amazonaws.com/travis-ci.kubra.io/build.tar.gz | tar -xvz -C /tmp/scripts
 
